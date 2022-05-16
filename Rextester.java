903b54fc23ee0b2f1a8432de9b135b08c6d7ea0d
Nielmesherep <105643756+Nielmesherep@users.noreply.github.com>
2022-05-16 19:31:03 +0800

    Add files via upload

---
+++ Rextester.java
@@ -0,0 +1,29 @@
+package Game;
+
+class harumiz {
+
+  public static void main(String[] args) {
+  {
+  
+        String s1 = "Hello";
+        String s2 = "FreeIT";
+         String s4 = "Hello";
+
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+        String result=s1 + s2;
+        System.out.println("s3 = "+result );
+
+       if(s4==s1)
+        {
+            System.out.println("s4 is equal to s1");
+        }
+        else
+        {
+            System.out.println("s4 is not equal to s1");
+        }
+    }
+}
+
+  }
